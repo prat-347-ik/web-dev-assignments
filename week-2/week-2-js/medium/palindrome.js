8946fa245833fd1b6bfff851938645333a404d64
prat-347-ik <chavanpratik2005@gmail.com>
2025-06-25 17:19:35 +0530

    solved

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -4,7 +4,50 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  str=str.replace(/[^a-z]/gi, '');
+  /*/[^a-z]/gi
+
+    [^a-z]: Match anything that's not a letter
+
+    g: global (all matches)
+
+    i: case-insensitive*/
+
+    
+  str=str.replace(/\s+/g,'');
+    /* /\s+/g is a regular expression:
+
+    \s matches any whitespace character (spaces, tabs, newlines)
+
+    + means one or more of those
+
+    g is for global, so it replaces all occurrences*/
+
+  let flag=0;/*assuming the string is a palindrome*/
+  let i=0;
+  let j=str.length-1;
+  while(i<j)
+  {
+    if(str[i].toLowerCase()===str[j].toLowerCase())
+    {
+      i++;
+      j--;
+    }
+    else 
+      {
+        flag=1;/*string is not a palindrome*/
+        break;
+      }
+  }
+  
+  if (flag===0)
+    return true;
+  else 
+    return false;
+      
+
+  
+  
 }
 
 module.exports = isPalindrome;
